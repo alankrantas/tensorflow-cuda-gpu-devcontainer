79115c52298c4e0ebe4e7f07b29455dddd27bb3b
Alan Wang <krantas@gmail.com>
2024-05-01 11:04:17 +0800

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,10 +1,8 @@
 # update system
 apt update
 apt upgrade -y
-# install Linux tools and Python 3
-apt install -y software-properties-common wget python3-dev python3-pip python3-wheel python3-setuptools
+# install Linux tools and Python 3 libs
+apt install -y software-properties-common python3-dev python3-pip python3-wheel python3-setuptools
 # install Python packages
 python3 -m pip install --upgrade pip
-pip3 install --user --extra-index-url https://pypi.nvidia.com -r .devcontainer/requirements.txt
-# clean up
-pip3 cache purge
+pip3 install -r .devcontainer/requirements.txt --user --extra-index-url https://pypi.nvidia.com
